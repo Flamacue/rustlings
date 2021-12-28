4de9905321f942af38515ee954bc598cac6d4619
Adam K. Sumner <asumner101@gmail.com>
2021-12-28 09:54:37 -0600

    my initial solutions

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -26,8 +26,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -41,6 +39,27 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            return Err(ParsePersonError::Empty);
+        }
+
+        let tokens: Vec<&str> = s.split(',').collect();
+        if tokens.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+
+        let name = tokens[0];
+        if name.is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+        let age = tokens[1]
+            .parse::<usize>()
+            .map_err(ParsePersonError::ParseInt)?;
+
+        Ok(Person {
+            age: age,
+            name: String::from(name),
+        })
     }
 }
 
--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -3,7 +3,9 @@
 // Basically, this is the same as From. The main difference is that this should return a Result type
 // instead of the target type itself.
 // You can read more about it at https://doc.rust-lang.org/std/convert/trait.TryFrom.html
+
 use std::convert::{TryFrom, TryInto};
+use std::num::TryFromIntError;
 
 #[derive(Debug, PartialEq)]
 struct Color {
@@ -21,7 +23,11 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
+impl From<TryFromIntError> for IntoColorError {
+    fn from(_: TryFromIntError) -> Self {
+        IntoColorError::IntConversion
+    }
+}
 
 // Your task is to complete this implementation
 // and return an Ok result of inner type Color.
@@ -33,9 +39,14 @@ enum IntoColorError {
 // Also note that correct RGB color values must be integers in the 0..=255 range.
 
 // Tuple implementation
+
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let red = u8::try_from(tuple.0)?;
+        let green = u8::try_from(tuple.1)?;
+        let blue = u8::try_from(tuple.2)?;
+        Ok(Color { red, green, blue })
     }
 }
 
@@ -43,6 +54,10 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let red = u8::try_from(arr[0])?;
+        let green = u8::try_from(arr[1])?;
+        let blue = u8::try_from(arr[2])?;
+        Ok(Color { red, green, blue })
     }
 }
 
@@ -50,6 +65,13 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+        let red = u8::try_from(slice[0])?;
+        let green = u8::try_from(slice[1])?;
+        let blue = u8::try_from(slice[2])?;
+        Ok(Color { red, green, blue })
     }
 }
 
